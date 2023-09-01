409e2afc40671683f8ed4a8724497a2cf3c208cf
rudskaya irina <irarudskaya5@gmail.com>
2023-09-02 01:02:27 +0300

    All commits

---
+++ src/main/java/ru/skypro/lesson/springboot/EmployeeApplication/security/model/AuthUser.java
@@ -0,0 +1,26 @@
+package ru.skypro.lesson.springboot.EmployeeApplication.security.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "auth_user")
+public class AuthUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    private String password;
+    @ManyToMany(fetch = FetchType.EAGER)
+    private List<UserRole> userRoles;
+}
---
+++ src/main/java/ru/skypro/lesson/springboot/EmployeeApplication/security/model/UserRole.java
@@ -0,0 +1,19 @@
+package ru.skypro.lesson.springboot.EmployeeApplication.security.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "user_role")
+public class UserRole {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String roles;
+
+}
